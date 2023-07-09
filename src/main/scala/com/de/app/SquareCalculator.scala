42cdd043c7dd45a8048cad841da23e5807789cdd
manjushreehs <manjushree929@gmail.com>
2023-07-09 21:02:32 +0530

    Added the required comments

--- src/main/scala/com/de/app/SquareCalculator.scala
+++ src/main/scala/com/de/app/SquareCalculator.scala
@@ -1,5 +1,8 @@
 package com.de.app
 
+/* Changes are made to the Square Calucluator*/
+/* Check for these comments as part of the commit */
+
 object SquareCalculator {
   def square(x: Int) = {
     x * x
