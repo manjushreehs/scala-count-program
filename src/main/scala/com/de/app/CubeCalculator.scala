0976a84476215a15813ab3a68ec3c66d847920bf
manjushreehs <manjushree929@gmail.com>
2023-07-09 21:27:04 +0530

    Updated CubeCalculator

--- src/main/scala/com/de/app/CubeCalculator.scala
+++ src/main/scala/com/de/app/CubeCalculator.scala
@@ -1,5 +1,7 @@
 package com.de.app
-
+/* Changes are made to the Cube Calculator*/
+/* Check for these comments as part of the commit */
+/*These are are changes as part of feature/CubeCalculator */
 object CubeCalculator extends App {
   def cube(x: Int) = {
     x * x * x
